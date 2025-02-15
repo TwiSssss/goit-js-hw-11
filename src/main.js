240ff85d2e8f1ce8a6ff9e50aeef636d830d8706
Vlad Tsokota <v.tsokota.69@gmail.com>
2025-02-15 17:12:31 +0200

    commit message

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { getImages } from "./js/pixabay-api";
+import { photosTemplate } from "./js/render-functions";
+
+let lightbox = new SimpleLightbox(".gallery-item a", {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+const refs = {
+    gallery: document.querySelector(".gallery"),
+    searchBtn: document.querySelector(".search-btn"),
+    searchInput: document.querySelector(".search-input"),
+    loader: document.querySelector(".loader"),
+};
+
+refs.searchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const wordKey = refs.searchInput.value.trim();
+    if (!wordKey) {
+        return;
+    }
+    refs.searchInput.value = "";
+    refs.loader.style.display = "inline-block";
+    getImages(wordKey)
+        .then((images) => {
+            if (images.length === 0) {
+                iziToast.error({
+                    message: "Sorry, there are no images matching your search query. Please try again!",
+                    position: "topRight",
+                });
+            } else {
+                refs.gallery.innerHTML = photosTemplate(images);
+                lightbox.refresh();
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            return [];
+        })
+        .finally(() => {
+            refs.loader.style.display = "none";
+        });
+});
